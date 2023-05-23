448f52a60d13d5fd36fb3d51ccdd411d9b524710
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2023-05-23 16:47:27 -0300

    Create Trabalhando Com Funcoes.js

---
+++ Modulo01-Introducao-A-Programacao/Trabalhando Com Funcoes.js
@@ -0,0 +1,25 @@
+//01 - Hello World
+/*Vamos reproduzir o exercício clássico do Hello World!
+Crie uma função chamada newFunction() que retorne o texto "Hello World".*/
+
+function newFunction() {
+    return "Hello World";
+};
+
+//02 - Sua Idade
+/*Crie uma função chamada minhaIdade que retorne um número que represente a sua idade.*/
+function minhaIdade() {
+    return 22;
+};
+
+//03 - Retorna Time
+/*Escreva uma função chamada timeDeFutebol(time) que recebe o nome de um time de futebol e retorna esse nome.*/
+function timeDeFutebol(time){
+  return time;
+};
+
+//04 - Sua Idade (com parâmetro)
+/*Escreva uma função chamada retornaIdade que recebe um número como parâmetro e retorna esse número.*/
+function retornaIdade(idade){
+  return idade;
+};
